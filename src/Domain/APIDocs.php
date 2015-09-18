6f7b296c8728f3f8d36a5f2c3f25a4ea95013486
Sean Prunka <sprunka@gmail.com>
2015-09-18 13:20:56 -0400

    Updated to only retrieve only functional endpoints.

--- src/Domain/APIDocs.php
+++ src/Domain/APIDocs.php
@@ -11,20 +11,22 @@ class APIDocs implements DomainInterface
     {
         // TODO: Find/Research a method for auto-generating this information from Routes.
         // This method of documenting the API is *not* scalable.
+        // TODO: Uncomment Endpoint descriptions as work is completed.
+
         return (new Payload)
             ->withStatus(Payload::OK)
             ->withOutput([
                 'Employee' => [
                     'List Shifts'=>'GET /myshifts[/{employeeId}]',
-                    'List Coworkers'=>'GET /mycoworkers[/{employeeId}]',
-                    'List Hours Worked'=>'GET /myhours[/{employeeId}]',
+                    //'List Coworkers'=>'GET /mycoworkers[/{employeeId}]',
+                    //'List Hours Worked'=>'GET /myhours[/{employeeId}]',
                     'List Manager Contact'=>'GET /mymanagers[/{employeeId}]'
                 ],
                 'Manager' => [
-                    'Create a shifts'=>'POST /create-shift[/{managerId}/{employeeId}/{shift}]',
-                    'List Shifts by time frame'=>'GET /list-shifts[/{managerId}/{startTime}/{endTime}]',
-                    'Update a given shift'=>'PUT /update-shift[/{managerId}/{shiftId}/{shift}]',
-                    'Assign a shift to an employee'=>'PUT /assign-shift[/{managerId}/{shiftId}/{employeeId}]',
+                    //'Create a shifts'=>'POST /create-shift[/{managerId}/{employeeId}/{shift}]',
+                    //'List Shifts by time frame'=>'GET /list-shifts[/{managerId}/{startTime}/{endTime}]',
+                    //'Update a given shift'=>'PUT /update-shift[/{managerId}/{shiftId}/{shift}]',
+                    //'Assign a shift to an employee'=>'PUT /assign-shift[/{managerId}/{shiftId}/{employeeId}]',
                     'List Employee Info'=>'GET /employee-data[/{managerId}/{employeeId}]'
                 ],
                 'NOTES' => 'the variable "shift" should be an object/array containing the details of the shift'
