f6a32d781b936bec36b2a3dc64ca2bf9018e58e0
Sean Prunka <sprunka@gmail.com>
2015-09-23 15:16:38 -0400

    moved the query inside the if
    This way it only attempts the query when it has an employee ID.

--- src/Domain/Employee/Shifts.php
+++ src/Domain/Employee/Shifts.php
@@ -18,18 +18,23 @@ class Shifts implements DomainInterface
 
         if (!empty($input['employeeId'])) {
             $employeeId = $input['employeeId'];
+
+            // TODO: create User Story clarification to detail what shift details are required.
+            // Is this just for upcoming shifts, is this all shifts, all shifts not yet worked, etc?
+            // What other info should be displayed? Should this return the manager as well?
+            // If so, by name? by ID? With contact (see related User Story.)
+            // Also, this could be used to additionally show coworkers (See relevant User Story)
+            // This could additionally sum up the hours worked.
+            // Current implementation displays all shifts in the db for teh given employee and only the start time, end time and break.
+            $query = $this->fpdo->from('shift')
+                ->where('employee_id', $employeeId);
+
+            foreach ($query as $row)
+            {
+                $output[] = ['start'=>$row['start_time'], 'end'=>$row['end_time'], 'break' => $row['break']];
+            }
         } else {
             $output['Input Error'] = 'You must supply your Employee credentials to request your shift information.';
-        }
-
-        // TODO: create User Story clarification to detail what shift details are required.
-        // For example, should this return the manager as well? If so, by name? by ID? With contact (see related User Story.)
-        $query = $this->fpdo->from('shift')
-            ->where('employee_id', $employeeId);
-
-        foreach ($query as $row)
-        {
-            $output[] = ['start'=>$row['start_time'], 'end'=>$row['end_time'], 'break' => $row['break']];
         }
 
         return (new Payload)
